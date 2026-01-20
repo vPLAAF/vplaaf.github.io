9127d771037a491e6fdbd976a7e3d13359565e8c
0x00A0 <xinrui_w@outlook.com>
2026-01-20 01:29:42 +0100

    map update

--- src/components/CookieConsentBanner.tsx
+++ src/components/CookieConsentBanner.tsx
@@ -44,11 +44,11 @@ export default function CookieConsentBanner() {
                         <Dialog.Title className="text-xl font-semibold text-slate-100">
                             Policy
                         </Dialog.Title>
-                        <Dialog.Description className="mt-2 text-sm text-slate-200">
+                        <Dialog.Description className="mt-2 text-xs text-slate-200">
                             Enter this website means that you have read and agreed to the following policy: <br/>
                             vPLAAF is an entirely fictional virtual aviation organization created solely for flight simulation and online entertainment. vPLAAF has no affiliation, subordination, cooperation, or authorization relationship with the real People’s Liberation Army Air Force or with any governmental, military, or commercial entity. All missions, structures, and designations are fictional and do not represent any real-world military unit, organization, or stance. All data and procedures conducted by this website and this organization must not be used for any real-world aviation or military purposes, and do not constitute any form of real-world flight training or operational reference.
                         </Dialog.Description>
-                        <p className="mt-1 text-sm text-slate-400">
+                        <p className="mt-1 text-xs text-slate-400">
                             进入本网站即表示您已阅读并同意以下政策：<br/>
                             vPLAAF虚拟中国空军为完全虚构的虚拟飞行组织， 仅以飞行模拟与网络娱乐为目的。vPLAAF 与现实中的中国人民解放军空军、 任何国家机关、军事单位或商业机构不存在任何形式的隶属、从属、合作或授权关系， 所有任务编制与设定均为虚构，不代表任何现实军事单位、组织或立场。 本网站及本组织的全部程序与数据均为虚拟不得用于任何现实航空或军事行为， 不构成任何形式的现实飞行训练或作战参考。
                         </p>
@@ -56,10 +56,10 @@ export default function CookieConsentBanner() {
                         <Dialog.Title className="text-xl font-semibold text-slate-100">
                             Privacy
                         </Dialog.Title>
-                        <Dialog.Description className="mt-2 text-sm text-slate-200">
+                        <Dialog.Description className="mt-2 text-xs text-slate-200">
                             This website uses cookies to ensure basic functionality and improve user experience.
                         </Dialog.Description>
-                        <p className="mt-1 text-sm text-slate-400">
+                        <p className="mt-1 text-xs text-slate-400">
                             本网站使用 Cookie 以确保基本功能并提升使用体验。
                         </p>
 
